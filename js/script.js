40fb52c89eaf03ed0a4f3b0da0b2b802b1a13e1d
Mohamed ali SAAFI <saafym@gmail.com>
2022-02-05 19:10:36 +0100

    displayhourtwo

--- js/script.js
+++ js/script.js
@@ -2,19 +2,24 @@
 const CONTENT = document.querySelector('#content');
 
 function updateTime (){
-    let d = new Date();
-    let second = d.getSeconds();
-    let time = d.toLocaleTimeString("fr");
-    if (second%3===0){
-       CONTENT.style.color= "red";
-       CONTENT.style.fontweight="bold";
-    } else{
-        CONTENT.style.color= "blue";
-        CONTENT.style.fontweight="normal";
-    }
-
-    CONTENT.innerText = time;
+   
+    setInterval(function changecolfo(){
+        let d = new Date();
+        let time = d.toLocaleTimeString("fr");
+        CONTENT.innerText = time;
+        let second = d.getSeconds();
+        if (second%3===0){
+        /*let el= document.styleSheets;
+       console.log (el);
+       el.StyleSheetList[1].cssRules[0].cssText=".para { color: red; font-weight:bold; }";
+       
+      let text= document.querySelector('.para');*/
+      CONTENT.setAttribute('class','para');
+    }else CONTENT.removeAttribute('class','para');
+    
+    }, 1000);  
+  
 }
-setInterval(updateTime, 1000);
+
 
 window.addEventListener('load', updateTime());
